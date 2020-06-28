35dd02023257c5f657f7a60bb880c4941dcacac8
xdeepakv <deepak.m.shrma@gmail.com>
2020-06-29 03:09:24 +0800

    updated vs_code publish

--- scripts/vs_code.js
+++ scripts/vs_code.js
@@ -12,36 +12,45 @@ async function main() {
   const packageJSON = JSON.parse(
     (await rf(join(process.cwd(), "package.json"))).toString()
   );
-  const pluck = (obj, keys) =>
+  const pluck = (obj, keys, def = {}) =>
     keys.reduce((m, key) => {
       m[key] = obj[key];
       return m;
     }, {});
   const vsCode = JSON.stringify(
-    pluck(packageJSON, [
-      "name",
-      "description",
-      "version",
-      "private",
-      "repository",
-      "keywords",
-      "author",
-      "license",
-      "bugs",
-      "icon",
-      "homepage",
-      "displayName",
-      "publisher",
-      "engines",
-      "categories",
-      "contributes",
-    ])
+    pluck(
+      packageJSON,
+      [
+        "name",
+        "description",
+        "version",
+        "private",
+        "repository",
+        "keywords",
+        "author",
+        "license",
+        "bugs",
+        "icon",
+        "homepage",
+        "displayName",
+        "publisher",
+        "engines",
+        "categories",
+        "contributes",
+      ],
+      {
+        devDependencies: {
+          "@types/vscode": "^1.34.0",
+          vsce: "^1.77.0",
+        },
+      }
+    )
   );
   await ex("npm run code-gen ");
   await rn("package.json", "_package.json");
   await wf("package.json", vsCode);
   if (process.argv.includes("--publish")) {
-    await ex("yarn vsce publish -p $TOKEN");
+    await ex("yarn vsce publish -p $VS_TOKEN");
   } else {
     await ex("yarn vsce package");
   }
